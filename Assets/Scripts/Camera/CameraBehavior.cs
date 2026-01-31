0bab334e01745e6cbe0e386f69842a6d76ede1e8
Walyn <walyncurtis@gmail.com>
2026-01-31 08:57:52 -0600

    Removes unneeded variables

--- Assets/Scripts/Camera/CameraBehavior.cs
+++ Assets/Scripts/Camera/CameraBehavior.cs
@@ -8,8 +8,6 @@ public class CameraBehavior : MonoBehaviour
 
     public float rotateSpeed = 180f;
 
-    public Transform yaw, pitch;
-
     private void Start()
     {
         look = InputSystem.actions.FindAction("Look");
@@ -19,10 +17,10 @@ public class CameraBehavior : MonoBehaviour
     {
         transform.position = player.position;
         Vector2 l = look.ReadValue<Vector2>() * rotateSpeed;
-        float rotX = yaw.localEulerAngles.y + l.x * Time.deltaTime;
-        float rotY = yaw.localEulerAngles.x + l.y * Time.deltaTime;
+        float rotX = transform.localEulerAngles.y + l.x * Time.deltaTime;
+        float rotY = transform.localEulerAngles.x + l.y * Time.deltaTime;
         if (rotY > 180f) rotY = Mathf.Clamp(rotY, 360f - 80f, 360f);
         else rotY = Mathf.Clamp(rotY, -1f, 80f);
-        yaw.rotation = Quaternion.Euler(rotY, rotX, 0f);
+        transform.rotation = Quaternion.Euler(rotY, rotX, 0f);
     }
 }
