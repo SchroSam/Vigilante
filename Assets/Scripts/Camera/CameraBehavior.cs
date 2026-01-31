51c39fcbec1c22665c8dad67fc26532569eab62e
Walyn <walyncurtis@gmail.com>
2026-01-31 10:02:37 -0600

    Removing from lockon list works safely

--- Assets/Scripts/Camera/CameraBehavior.cs
+++ Assets/Scripts/Camera/CameraBehavior.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class CameraBehavior : MonoBehaviour
 {
     public Transform player;
-    InputAction look, changeLockon;
+    InputAction look, changeLockon, debugKill;
     public static Transform lockOn;
 
     public float rotateSpeed = 180f;
@@ -17,6 +17,7 @@ public class CameraBehavior : MonoBehaviour
     {
         look = InputSystem.actions.FindAction("Look");
         changeLockon = InputSystem.actions.FindAction("ChangeLockon");
+        debugKill = InputSystem.actions.FindAction("DebugKill");
     }
 
     void Update()
@@ -43,6 +44,11 @@ public class CameraBehavior : MonoBehaviour
                 lockOn = lockables[iter];
             }
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lockOn.position - player.position) * Quaternion.Euler(30, 0, 0), Time.deltaTime * 1000f);
+
+            if (debugKill.WasPressedThisFrame())
+            {
+                RemoveFromLockables(lockOn);
+            }
         }
         transform.position = player.position;
     }
@@ -53,6 +59,11 @@ public class CameraBehavior : MonoBehaviour
         lockables.Remove(t);
         if (refresh)
         {
+            if (lockables.Count == 0)
+            {
+                lockOn = null;
+                return;
+            }
             iter %= lockables.Count;
             lockOn = lockables[iter];
         }
