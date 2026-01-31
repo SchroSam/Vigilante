b8f465be8eef62b6c97b8a424ae65766390a1e48
Samuel Scott Schroeder <schrosam@gmail.com>
2026-01-31 14:09:16 -0600

    1/2 way through fight behavior

---
+++ Assets/Scripts/Characters/AI/GroupManager.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupManager : MonoBehaviour
+{
+
+    public List<Nav> members;
+    Nav activeFighter;
+
+    void Start()
+    {
+        foreach (Transform child in transform) 
+        { 
+            members.Add(child.GetComponent<Nav>());
+        }
+    }
+
+    public void AssignAttacker()
+    {
+        bool fightingAlready = false;
+
+        for(int i = 0; i < members.Count; i++)
+        {
+            if(members[i].state == Nav.E_State.Fight)
+            {
+                activeFighter = members[i];
+                fightingAlready = true;
+                break;
+            }
+        }
+
+        if(!fightingAlready){
+
+            members[0].StateChange(Nav.E_State.Fight);
+            activeFighter = members[0];
+            
+            for(int i = 1; i < members.Count; i++)
+            {
+                members[i].StateChange(Nav.E_State.Waiting);
+            }
+        }
+
+        else
+        {
+            activeFighter.StateChange(Nav.E_State.Waiting);
+
+            activeFighter = members[Random.Range(0, members.Count - 1)];
+
+            activeFighter.StateChange(Nav.E_State.Fight);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Characters/AI/Nav.cs
+++ Assets/Scripts/Characters/AI/Nav.cs
@@ -5,24 +5,31 @@ using UnityEngine.AI;
 public class Nav : MonoBehaviour
 {
     [System.Serializable]
-    public enum E_State{Idle, Seek, Fight, Die}
+    public enum E_State{Idle, Seek, Fight, Die, Waiting}
 
     NavMeshAgent m_Agent;
     CharacterBehavior behavior;
     private GameObject player;
     InputPackage nextInput;
     Vector3 movDir3;
-    public GameObject pusher;
-    bool pushed = false;
+    public GroupManager group;
+    private GameObject pusher;
+    
 
+    bool pushed = false;
     public float pushDist = 0.3f;
     public float pushReduce = 2f;
+
+    bool engaging = false;
+    bool striking = false;
+    public float attackRange = 0.3f;
 
     
     public E_State state = E_State.Idle;
 
     void Start()
     {
+        group = transform.parent.GetComponent<GroupManager>();
         player = FindFirstObjectByType<Player>().gameObject;
         behavior = GetComponent<CharacterBehavior>();
         m_Agent = GetComponent<NavMeshAgent>();
@@ -30,7 +37,7 @@ public class Nav : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(state == E_State.Seek)
         {
@@ -54,6 +61,41 @@ public class Nav : MonoBehaviour
                 pushed = false;
             }
         }
+
+        else if(state == E_State.Waiting)
+        {
+            //add propper waiting behavior
+            nextInput.action = "Block";
+
+        }
+
+        else if(state == E_State.Fight)
+        {
+            if(Vector3.Distance(transform.position, player.transform.position) > attackRange && !striking)
+            {
+                nextInput.action = "Move";
+
+                if(m_Agent.steeringTarget != null && !pushed)
+                    movDir3 = (m_Agent.steeringTarget - new Vector3(transform.position.x, transform.position.y, transform.position.z)).normalized;
+                else if (pushed)
+                    movDir3 = (transform.position - pusher.transform.position).normalized + m_Agent.steeringTarget.normalized * pushReduce;
+
+                if(pushed && Vector3.Distance(transform.position, pusher.transform.position) > pushDist)
+                {
+                    pushed = false;
+                }
+            }
+
+            else if(Vector3.Distance(transform.position, player.transform.position) <= attackRange && !striking)
+            {
+                nextInput.action = "Attack";
+                
+            }
+
+
+
+
+        }
     }
 
     public void StateChange(E_State nextState)
@@ -61,9 +103,29 @@ public class Nav : MonoBehaviour
         if(state == E_State.Seek)
         {
             movDir3 = Vector3.zero;
+            nextInput.movedir = Vector2.zero;
+            nextInput.action = "Idle";
+
+            behavior.Process(nextInput);
         }
 
         state = nextState;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "FightRadius")
+        {
+            group.AssignAttacker();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.name == "FightRadius")
+        {
+            StateChange(E_State.Seek);
+        }
     }
 
 
