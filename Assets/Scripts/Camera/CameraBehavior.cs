1ceaca347bace64dc57da48bb66848f067f11f7e
Walyn <walyncurtis@gmail.com>
2026-01-31 09:50:16 -0600

    Adds switching between targets

--- Assets/Scripts/Camera/CameraBehavior.cs
+++ Assets/Scripts/Camera/CameraBehavior.cs
@@ -1,23 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class CameraBehavior : MonoBehaviour
 {
     public Transform player;
-    InputAction look;
-    public Transform lockOn;
+    InputAction look, changeLockon;
+    public static Transform lockOn;
 
     public float rotateSpeed = 180f;
+
+    public static List<Transform> lockables = new List<Transform>();
+    public static int iter = 0;
 
     private void Start()
     {
         look = InputSystem.actions.FindAction("Look");
+        changeLockon = InputSystem.actions.FindAction("ChangeLockon");
     }
 
     void Update()
     {
         if (lockOn == null)
         {
+            if (lockables.Count > 0)
+            {
+                lockOn = lockables[0];
+                iter = 0;
+            }
             Vector2 l = look.ReadValue<Vector2>() * rotateSpeed;
             float rotX = transform.localEulerAngles.y + l.x * Time.deltaTime;
             float rotY = transform.localEulerAngles.x + l.y * Time.deltaTime;
@@ -26,8 +36,25 @@ public class CameraBehavior : MonoBehaviour
             transform.rotation = Quaternion.Euler(rotY, rotX, 0f);
         } else
         {
+            if (changeLockon.WasPressedThisFrame())
+            {
+                iter += 1;
+                iter %= lockables.Count;
+                lockOn = lockables[iter];
+            }
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lockOn.position - player.position) * Quaternion.Euler(30, 0, 0), Time.deltaTime * 1000f);
         }
         transform.position = player.position;
+    }
+
+    public static void RemoveFromLockables(Transform t)
+    {
+        bool refresh = lockables[iter] == t;
+        lockables.Remove(t);
+        if (refresh)
+        {
+            iter %= lockables.Count;
+            lockOn = lockables[iter];
+        }
     }
 }
