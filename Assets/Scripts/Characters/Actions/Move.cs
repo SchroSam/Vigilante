144f631786a28182dfb65efd1b6f9bd663189ebc
Walyn <walyncurtis@gmail.com>
2026-01-30 21:50:59 -0600

    Cleanup and fixing things

--- Assets/Scripts/Characters/Actions/Move.cs
+++ Assets/Scripts/Characters/Actions/Move.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class IdleAction : CharAction
+public class Move : CharAction
 {
     public override string GetNextAction(InputPackage input)
     {
-        if (input.action == "Move") return input.action;
+        if (input.action != "Move") return input.action;
         return "";
     }
 
