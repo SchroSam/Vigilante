d1ba76d4755c2b65862a9c08023f996e3c201712
Walyn <walyncurtis@gmail.com>
2026-01-31 09:29:52 -0600

    Adds lockon camera behavior

--- Assets/Scripts/Camera/CameraBehavior.cs
+++ Assets/Scripts/Camera/CameraBehavior.cs
@@ -5,6 +5,7 @@ public class CameraBehavior : MonoBehaviour
 {
     public Transform player;
     InputAction look;
+    public Transform lockOn;
 
     public float rotateSpeed = 180f;
 
@@ -15,12 +16,18 @@ public class CameraBehavior : MonoBehaviour
 
     void Update()
     {
+        if (lockOn == null)
+        {
+            Vector2 l = look.ReadValue<Vector2>() * rotateSpeed;
+            float rotX = transform.localEulerAngles.y + l.x * Time.deltaTime;
+            float rotY = transform.localEulerAngles.x + l.y * Time.deltaTime;
+            if (rotY > 180f) rotY = Mathf.Clamp(rotY, 360f - 80f, 360f);
+            else rotY = Mathf.Clamp(rotY, -1f, 80f);
+            transform.rotation = Quaternion.Euler(rotY, rotX, 0f);
+        } else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lockOn.position - player.position) * Quaternion.Euler(30, 0, 0), Time.deltaTime * 1000f);
+        }
         transform.position = player.position;
-        Vector2 l = look.ReadValue<Vector2>() * rotateSpeed;
-        float rotX = transform.localEulerAngles.y + l.x * Time.deltaTime;
-        float rotY = transform.localEulerAngles.x + l.y * Time.deltaTime;
-        if (rotY > 180f) rotY = Mathf.Clamp(rotY, 360f - 80f, 360f);
-        else rotY = Mathf.Clamp(rotY, -1f, 80f);
-        transform.rotation = Quaternion.Euler(rotY, rotX, 0f);
     }
 }
