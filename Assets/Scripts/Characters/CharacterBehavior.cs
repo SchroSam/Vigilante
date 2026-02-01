9ed18942dfd5aaccfbfd285ae2d90442c3ba400f
Walyn <walyncurtis@gmail.com>
2026-01-31 20:06:40 -0600

    Fixed some problems with blocking, finished anims

--- Assets/Scripts/Characters/CharacterBehavior.cs
+++ Assets/Scripts/Characters/CharacterBehavior.cs
@@ -12,13 +12,41 @@ public class CharacterBehavior : MonoBehaviour
     public Animator animator;
 
     public string forcedAct;
+    bool hasForcedAct = false;
 
     public int maxHealth = 100;
     int health;
 
     public bool blocking;
     public Vector2Int dir;
-    public List<Collider> objectsHit = new List<Collider>();
+    public List<Collider> objectsHit
+    {
+        get {
+            print("objectsHit being accessed, it has " + _objectsHit.Count + " items");
+            for (int i = 0; i < _objectsHit.Count; i++)
+            {
+                print(i + " : " + _objectsHit[i]);
+            }
+            return _objectsHit;
+        }
+        set
+        {
+            _objectsHit = value;
+        }
+    }
+    List<Collider> _objectsHit = new List<Collider>();
+
+    public void ClearObjectsHit()
+    {
+        print("clearing objects hit");
+        _objectsHit.Clear();
+    }
+
+    public void AddToObjectsHit(Collider c)
+    {
+        print("adding " + c);
+        _objectsHit.Add(c);
+    }
 
     private void Start()
     {
@@ -30,22 +58,27 @@ public class CharacterBehavior : MonoBehaviour
             c.controller = controller;
             c.animator = animator;
         }
-        SwitchAction(defaultAct, new InputPackage());
+        curAct = defaultAct;
+        defaultAct.Enter(new InputPackage());
         health = maxHealth;
     }
 
     public void Reset()
     {
-        SwitchAction(defaultAct, new InputPackage());
+        curAct = defaultAct;
+        defaultAct.Enter(new InputPackage());
         health = maxHealth;
         animator.Play("Idle");
     }
 
     public void HandleHit(HitInfo hit)
     {
-        if (blocking && hit.dir == new Vector2Int(-hit.dir.x, hit.dir.y))
+        print(hit.dir);
+        print(new Vector2Int(-dir.x, dir.y));
+        if (blocking && hit.dir == new Vector2Int(-dir.x, dir.y))
         {
             print("blocked");
+            hit.source.SetForcedAct("Hurt");
         } else
         {
             print("hit");
@@ -55,23 +88,33 @@ public class CharacterBehavior : MonoBehaviour
 
     public void TakeDamage(int dmg)
     {
-        if (curAct == acts["Die"]) return;
+        if (curAct == acts["Die"]) {
+            return;
+        }
         health -= dmg;
         if (health <= 0)
         {
             health = 0;
-            forcedAct = "Die";
+            SetForcedAct("Die");
         } else
         {
-            forcedAct = "Hurt";
+            SetForcedAct("Hurt");
         }
+    }
+
+    public void SetForcedAct(string act)
+    {
+        forcedAct = act;
+        hasForcedAct = true;
     }
 
     public void Process(InputPackage input)
     {
         string nextAct = "";
-        if (forcedAct != null)
+        if (hasForcedAct)
         {
+            hasForcedAct = false;
+            print("forced act: " + forcedAct);
             nextAct = forcedAct;
             forcedAct = null;
         } else
@@ -93,7 +136,8 @@ public class CharacterBehavior : MonoBehaviour
 
     void SwitchAction(CharAction nextAct, InputPackage input)
     {
-        if (curAct) curAct.Exit();
+        print("switching action from " + curAct);
+        curAct.Exit();
         curAct = nextAct;
         print("switching to " + curAct.gameObject.name);
         curAct.Enter(input);
