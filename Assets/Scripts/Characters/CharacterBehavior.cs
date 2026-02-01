fcf244e8adaba7387fc98d6e0b09965965f91262
Micah Graves <micah.graves91@gmail.com>
2026-02-01 13:32:55 -0600

    Maptest and sprite conversion

--- Assets/Scripts/Characters/CharacterBehavior.cs
+++ Assets/Scripts/Characters/CharacterBehavior.cs
@@ -97,7 +97,11 @@ public class CharacterBehavior : MonoBehaviour
             health = 0;
             forcedAct = "Die";
             //Lose screen function here
-            FindFirstObjectByType<LoseUI>().LoseScreen();
+            if (gameObject.tag == "Player")
+            {
+                FindFirstObjectByType<LoseUI>().LoseScreen();
+            }
+                
         } else
         {
             SetForcedAct("Hurt");
